0c0d85da3a458501a0e486d387c5d8b94e75ee0a
M Rasyid Ridha <rasyid.ridha@swiftsolutions.id>
2022-10-26 23:32:03 +0700

    [ADD] route

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/rasyidridha547/simple-fiber-http/config"
@@ -13,11 +14,27 @@ func main() {
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
+		now := time.Now()
+
 		return c.Status(fiber.StatusOK).JSON(fiber.Map{
-			"success": true,
-			"message": "Hello World!",
+			"success":       true,
+			"message":       "Hello World!",
+			"response_time": time.Since(now).String(),
 		})
 	})
 
+	app.Get("/user/:name", getName)
+
 	app.Listen(fmt.Sprintf(":%s", c.Port))
+}
+
+func getName(c *fiber.Ctx) error {
+	name := c.Params("name")
+
+	result := fmt.Sprintf("Hello, %+v", name)
+
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{
+		"status_code": fiber.StatusOK,
+		"message":     result,
+	})
 }
